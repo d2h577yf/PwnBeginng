bd004290fc145fbf59175c0b0554b15be09b24f2
d2h577yf <why20051028XY@icloud.com>
2024-09-25 20:13:03 +0800

    1

---
+++ NotEnoughTime.py
@@ -0,0 +1,31 @@
+from pwn import remote
+import re
+
+connect = remote("127.0.0.1", 50976)
+
+while True:
+    try:
+        expression = connect.recvline().decode().strip()
+        print(f"表达式: {expression}")
+
+        expression = expression.replace("/", "//")
+
+        if re.match(r"^[\d\+\-\*//() ]+$", expression):
+            try:
+                result = eval(expression)
+                print(f"结果: {result}")
+            except Exception as e:
+                print(f"错误表达式: {e}")
+                result = "Error"
+        else:
+            result = "无效"
+
+        connect.sendline(str(result))
+
+    except EOFError:
+        print("EoFError.")
+        break
+    except Exception as e:
+        print(f"EoFError: {e}")
+        break
+
